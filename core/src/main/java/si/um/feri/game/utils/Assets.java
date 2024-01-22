e8187ef22add93e627b9f6d20b94deb270fbf0c2
hristovass <sara.hristova@student.um.si>
2024-01-22 13:36:16 +0100

    Change assets

--- core/src/main/java/si/um/feri/game/utils/Assets.java
+++ core/src/main/java/si/um/feri/game/utils/Assets.java
@@ -7,10 +7,15 @@ import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 public class Assets {
 
     public static Texture bikeImg;
+    public static Texture bike1Img;
+    public static Texture bike2Img;
     public static Skin skin;
 
     public static void load() {
         bikeImg = new Texture("bike.png");
+        bike1Img=new Texture("bike1.png");
+        bike1Img=new Texture("bike2.png");
+
         //skin = new Skin(Gdx.files.internal("neon_skin/neon-ui.json"));
         skin = new Skin(Gdx.files.internal("default-skin/uiskin.json"));
     }
